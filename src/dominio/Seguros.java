06e098955963a66a0445bf30481d064793360603
facumarcati <facumarcati14@gmail.com>
2023-05-26 17:07:47 -0300

    Paquete Dominio, clases Seguros y SegurosDao

---
+++ src/dominio/Seguros.java
@@ -0,0 +1,66 @@
+package dominio;
+
+public class Seguros {
+	
+	private int ID;
+	private String descripcion;
+	private int tipoSeguro;
+	private float costoContratacion;
+	private float costoMaxAsegurado;
+	
+	public Seguros() {}
+	
+	public Seguros(int ide, String desc, int tiposeguro, float costCont, float costMaxAseg) {
+		this.ID = ide;
+		this.descripcion = desc;
+		this.tipoSeguro = tiposeguro;
+		this.costoContratacion = costCont;
+		this.costoMaxAsegurado = costMaxAseg;
+	}
+
+	public int getID() {
+		return ID;
+	}
+
+	public void setID(int iD) {
+		ID = iD;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public int getTipoSeguro() {
+		return tipoSeguro;
+	}
+
+	public void setTipoSeguro(int tipoSeguro) {
+		this.tipoSeguro = tipoSeguro;
+	}
+
+	public float getCostoContratacion() {
+		return costoContratacion;
+	}
+
+	public void setCostoContratacion(float costoContratacion) {
+		this.costoContratacion = costoContratacion;
+	}
+
+	public float getCostoMaxAsegurado() {
+		return costoMaxAsegurado;
+	}
+
+	public void setCostoMaxAsegurado(float costoMaxAsegurado) {
+		this.costoMaxAsegurado = costoMaxAsegurado;
+	}
+
+	@Override
+	public String toString() {
+		return ID + " - " + descripcion + " - " + tipoSeguro + " - " + costoContratacion + " - " + costoMaxAsegurado;
+	}
+	
+}
---
+++ src/dominio/SegurosDao.java
@@ -0,0 +1,150 @@
+package dominio;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import dominio.Seguros;
+
+public class SegurosDao {
+
+	private String host = "jdbc:mysql://localhost:3306/";
+	private String user = "root";
+	private String pass = "root";
+	private String dbName = "segurosgroup";
+
+	public int eliminarSeguro(int id)
+	{
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		int filas = 0;
+		Connection cn = null;
+		try
+		{
+			cn = DriverManager.getConnection(host+dbName, user,pass);
+			Statement st = cn.createStatement();
+			String query = "delete from seguros where id="+id;
+			filas=st.executeUpdate(query);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		return filas;
+	}
+
+	
+	public int agregarSeguro(Seguros seguros)
+	{
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		int filas=0;
+		Connection cn = null;
+		try
+		{
+			cn = DriverManager.getConnection(host+dbName, user,pass);
+			Statement st = cn.createStatement();
+			String query = "Insert into seguros(idSeguro, descripcion, idTipo, costoContratacion, costoAsegurado) values ('"+seguros.getID()+"','"+seguros.getDescripcion()+"','"+seguros.getTipoSeguro()+"','"+seguros.getCostoContratacion()+"','"+seguros.getCostoMaxAsegurado()+"')";
+			filas=st.executeUpdate(query);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		return filas;
+	}
+	
+	
+	public ArrayList<Seguros> obtenerSeguros() {
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		ArrayList<Seguros> lista = new ArrayList<Seguros>();
+		Connection conn = null;
+		try{
+			conn = DriverManager.getConnection(host + dbName, user, pass);
+			Statement st = conn.createStatement();
+			
+			ResultSet rs = st.executeQuery("Select idSeguro,descripcion,idTipo,costoContratacion,costoAsegurado FROM seguros");
+			
+			while(rs.next()){
+				
+				Seguros segurosRs = new Seguros();
+				segurosRs.setID(rs.getInt("idSeguro"));
+				segurosRs.setDescripcion(rs.getString("descripcion"));
+				segurosRs.setTipoSeguro(rs.getInt("idTipo"));
+				segurosRs.setCostoContratacion(rs.getFloat("costoContratacion"));
+				segurosRs.setCostoMaxAsegurado(rs.getFloat("costoAsegurado"));
+				
+				lista.add(segurosRs);
+			}
+			
+			conn.close();
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally{
+		
+		}
+		
+		return lista;
+	}
+
+	
+	public Seguros obtenerUnUsuario(int id)
+	{
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		Seguros seguro = new Seguros();
+		Connection con = null;
+		try{
+			con = DriverManager.getConnection(host + dbName, user, pass);
+			PreparedStatement miSentencia = con.prepareStatement("Select * from seguros where idSeguro = ?");
+			miSentencia.setInt(1, id); //Cargo el ID recibido
+			ResultSet resultado = miSentencia.executeQuery();
+			resultado.next();
+			
+			seguro.setID(resultado.getInt(1));
+			seguro.setDescripcion(resultado.getString(2));
+			seguro.setTipoSeguro(resultado.getInt(3));
+		    seguro.setCostoContratacion(resultado.getFloat(4));
+		    seguro.setCostoMaxAsegurado(resultado.getFloat(5));
+			
+		    con.close();
+		}
+		catch(Exception e)
+		{
+			System.out.println("Conexion fallida");
+		}
+		finally
+		{
+		}
+		return seguro;
+	}
+	
+}
